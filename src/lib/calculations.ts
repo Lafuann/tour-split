a60390c359b7cd4d51becb05585709af49abfa5f
Muhammad Naufal Fauzi <lafuanfauzi@gmail.com>
2025-12-26 09:53:57 +0700

    change format date on table

--- src/lib/calculations.ts
+++ src/lib/calculations.ts
@@ -97,7 +97,7 @@ export const formatCurrency = (amount: number): string => {
 export const formatDateTime = (date: Date): string => {
   return new Intl.DateTimeFormat("id-ID", {
     day: "2-digit",
-    month: "2-digit",
+    month: "short",
     year: "numeric",
     // hour: "2-digit",
     // minute: "2-digit",
